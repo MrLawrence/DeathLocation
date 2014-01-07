baf02f47df8efb415fc44a98e8478838de013a81
Mr. Lawrence <lawrence@server.fake>
2014-01-08 00:26:12 +0100

    added configuration file

--- src/bukkit/deathlocation/DeathListener.java
+++ src/bukkit/deathlocation/DeathListener.java
@@ -1,24 +1,48 @@
 package bukkit.deathlocation;
 
 import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class DeathListener implements Listener {
-	
+	private final JavaPlugin plugin;
+
+	public DeathListener(JavaPlugin plugin) {
+		this.plugin = plugin;
+	}
+
 	@EventHandler
-	 public void death(PlayerDeathEvent event) {
-	      Player deathPlayer = event.getEntity();
-	      Location deathLocation = deathPlayer.getLocation();
-	      World world = deathLocation.getWorld();
-	      String worldName = world.getName();
-	      int x = deathLocation.getBlockX();
-	      int y = deathLocation.getBlockY();
-	      int z = deathLocation.getBlockZ();
-	      String locationString = x + "|" + y + "|" + z;
-	      deathPlayer.sendMessage("You died on "+ worldName + " at " + locationString);
-	    }
+	public void death(PlayerDeathEvent event) {
+		Player player = event.getEntity();
+
+		Object[] formatArgs = getArgs(player.getLocation());
+
+		String configString = plugin.getConfig().getString("message");
+		String format = formatString(configString);
+
+		String message = String.format(format, formatArgs);
+		player.sendMessage(message);
+	}
+
+	private Object[] getArgs(Location location) {
+		String world = location.getWorld().getName();
+		String x = Integer.toString(location.getBlockX());
+		String y = Integer.toString(location.getBlockY());
+		String z = Integer.toString(location.getBlockZ());
+
+		Object[] args = { world, x, y, z };
+		return args;
+	}
+
+	private String formatString(String configString) {
+		String formatted;
+		formatted = configString.replace("$w", "%1$s");
+		formatted = formatted.replace("$x", "%2$s");
+		formatted = formatted.replace("$y", "%3$s");
+		formatted = formatted.replace("$z", "%4$s");
+		return formatted;
+	}
 }
--- src/bukkit/deathlocation/DeathLocation.java
+++ src/bukkit/deathlocation/DeathLocation.java
@@ -4,9 +4,12 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DeathLocation extends JavaPlugin {
-	private final DeathListener deathListener = new DeathListener();
+	private final DeathListener deathListener = new DeathListener(this);
+	
 	@Override
 	public void onEnable() {
+	    this.saveDefaultConfig();
+	    
 		PluginManager pm = getServer().getPluginManager();
 		pm.registerEvents(deathListener, this);
 	}

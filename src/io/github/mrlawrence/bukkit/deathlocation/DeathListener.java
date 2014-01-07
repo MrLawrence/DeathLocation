80ba00d8eea33c3fa8ab57193cdca5a15f8381ec
Mr. Lawrence <lawrence@server.fake>
2014-01-07 12:24:25 +0100

    initial commit

---
+++ src/io/github/mrlawrence/bukkit/deathlocation/DeathListener.java
@@ -0,0 +1,24 @@
+package io.github.mrlawrence.bukkit.deathlocation;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class DeathListener implements Listener {
+	
+	@EventHandler
+	 public void death(PlayerDeathEvent event) {
+	      Player deathPlayer = event.getEntity();
+	      Location deathLocation = deathPlayer.getLocation();
+	      World world = deathLocation.getWorld();
+	      String worldName = world.getName();
+	      int x = deathLocation.getBlockX();
+	      int y = deathLocation.getBlockY();
+	      int z = deathLocation.getBlockZ();
+	      String locationString = x + "|" + y + "|" + z;
+	      deathPlayer.sendMessage("You died on "+ worldName + " at " + locationString);
+	    }
+}
---
+++ src/io/github/mrlawrence/bukkit/deathlocation/DeathLocation.java
@@ -0,0 +1,17 @@
+package io.github.mrlawrence.bukkit.deathlocation;
+
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DeathLocation extends JavaPlugin {
+	private final DeathListener deathListener = new DeathListener();
+	@Override
+	public void onEnable() {
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(deathListener, this);
+	}
+
+	@Override
+	public void onDisable() {
+	}
+}
